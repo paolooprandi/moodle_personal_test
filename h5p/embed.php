500047505870337473ad75df38a041c95238c9d2
paolooprandi <paolo.oprandi@gmail.com>
2022-10-28 16:12:12 +0100

    add moove_ebvm theme

--- h5p/embed.php
+++ h5p/embed.php
@@ -1,5 +1,7 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
+// 
+// Paolo asks - should this directory exist here?
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
